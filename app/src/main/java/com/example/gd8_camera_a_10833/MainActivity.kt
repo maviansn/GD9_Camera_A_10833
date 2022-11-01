6da591021c154085cfa3421842469a96e850f7d0
maviansn <vannessatriagunawan@gmail.com>
2022-11-01 14:08:28 +0700

    add GD Acceleromotor

--- app/src/main/java/com/example/gd8_camera_a_10833/MainActivity.kt
+++ app/src/main/java/com/example/gd8_camera_a_10833/MainActivity.kt
@@ -1,34 +1,64 @@
 package com.example.gd8_camera_a_10833
 
-import android.annotation.SuppressLint
-import android.hardware.Camera
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.ImageButton
-import java.lang.Exception
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 
-class MainActivity : AppCompatActivity() {
-    private var mCamera: Camera? = null
-    private var mCameraView: CameraView? = null
+class MainActivity : AppCompatActivity() ,SensorEventListener{
+    private lateinit var sensorManager: SensorManager
+    private lateinit var square: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        try{
-            mCamera = Camera.open()
-        }catch (e: Exception){
-            Log.d("Error","Failed to get Camera" + e.message)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        square = findViewById(R.id.tv_square)
+        setUpSensorStuff()
+    }
+
+    fun setUpSensorStuff(){
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+                accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
         }
-        if(mCamera != null){
-            mCameraView = CameraView(this,mCamera!!)
-            val camera_view = findViewById<View>(R.id.FLCamera) as FrameLayout
-            camera_view.addView(mCameraView)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?){
+        if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+            val sides = event.values[0]
+            val upDown  = event.values[1]
+            square.apply {
+                rotationX = upDown * 3f
+                rotationY = sides * 3f
+                rotation = -sides
+                translationX = sides * -10
+                translationY = upDown * 10
+            }
+            val color = if(upDown.toInt() == 0 && sides.toInt() == 0) Color.GREEN else Color.RED
+            square.setBackgroundColor(color)
+            square.text = "up/down ${upDown.toInt()}\nleft/right ${sides.toInt()}"
         }
-        @SuppressLint("MissingInflatedId", "LocalSuppress")
-        val imageClose = findViewById<View>(R.id.imgClose) as ImageButton
-        imageClose.setOnClickListener {view: View? -> System.exit(0)}
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
     }
 }
